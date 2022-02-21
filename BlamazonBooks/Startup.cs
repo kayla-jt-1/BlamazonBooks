10a32c0a0f4aaf595fdc1b5c012381c8c288d52e
kaylajt-1 <kayla.telford@byu.edu>
2022-02-21 09:54:00 -0700

    Connection String

--- BlamazonBooks/Startup.cs
+++ BlamazonBooks/Startup.cs
@@ -2,6 +2,8 @@ using BlamazonBooks.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -15,10 +17,20 @@ namespace BlamazonBooks
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        public IConfiguration Configuration { get; set; }
+        public Startup (IConfiguration temp)
+        {
+            Configuration = temp;
+        }
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<BookstoreContext>
+            services.AddDbContext<BookstoreContext>(options =>
+           {
+               options.UseSqlite(Configuration["ConnectionString:BookstoreConnection"]);
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
