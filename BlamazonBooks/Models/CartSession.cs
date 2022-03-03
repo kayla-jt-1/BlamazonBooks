038d381b7be851ba038e7afc46742a6db44f5b7d
kaylajt-1 <kayla.telford@byu.edu>
2022-03-02 22:37:31 -0700

    CartSession

---
+++ BlamazonBooks/Models/CartSession.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlamazonBooks.Models
+{
+    public class CartSession
+    {
+    }
+}
